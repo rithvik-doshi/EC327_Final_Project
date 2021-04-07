fffc2f2fccd02ba25978c2dce218b5fdf5af2bd7
Muhammed Abdalla <muhabda@bu.edu>
2021-04-07 16:17:31 -0400

    Init.cpp main file

---
+++ Init.cpp
@@ -0,0 +1,15 @@
+// Copyright 2021 Muhammed Abdalla muhabda@bu.edu
+
+#include <iostream>
+#include <string>
+#include <array>
+#include <vector>
+#include<map>
+#include <movedef.h>
+
+using std::cout;
+using std::cin;
+
+int main(int argc, char** argv){
+  return 0;
+}
